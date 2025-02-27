5967044686a96c52d2e1e3ea228e95bfb78de0cd
praveen9655 <praveen965592@gmail.com>
2025-02-27 16:53:35 +0530

    V1.1

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://kcgifxoxllxruqldagkd.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtjZ2lmeG94bGx4cnVxbGRhZ2tkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA2NDUxNTksImV4cCI6MjA1NjIyMTE1OX0.GW3BJikTd8iPSeCjDCBH7qrkEgVtIeKiMWAJiwzrNnQ";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
